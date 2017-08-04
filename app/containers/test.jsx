a6b15926a7c5b4615a583a75e46e1b0736709d14
mr.keiki@gmail.com <mr.keiki@gmail.com>
2017-08-04 17:05:51 +0800

    添加material-ui示例样式

--- app/containers/test.jsx
+++ app/containers/test.jsx
@@ -1,40 +1,39 @@
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { createSelector } from 'reselect';
-import { testDispatch } from '../actions';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MaterialUI from '../components/MaterialUI';
 
 class TestPage extends React.Component {
-  constructor(props, context) {
+  constructor (props, context) {
     super(props, context);
+    this.state = {
+      items: []
+    };
   }
 
-  componentWillMount() {
-    const { dispatch } = this.props;
-    dispatch(testDispatch())
-  }
-
-  render() {
+  render () {
     return (
       <div>
-        test page
+        <MuiThemeProvider>
+          <MaterialUI />
+        </MuiThemeProvider>
       </div>
-    )
+    );
   }
 }
 
-
-const tagsSelector = function(state) {
+const tagsSelector = function (state) {
   return state.tagsPage;
 };
 
 const ContentPageSelector = createSelector(
   [tagsSelector],
-  (tags) => {
-    return {
-      tags
-    };
-  }
+  (tags) => (
+    { tags }
+  )
 );
 
 export default connect(ContentPageSelector)(withRouter(TestPage));
